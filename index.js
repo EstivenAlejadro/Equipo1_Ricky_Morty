3aed0bf8001e2e02f52bd259493d8ba2a8c04d49
Bxr3nd4 <Fiona5109@hotmail.com>
2023-06-24 09:40:56 -0500

    consumo API

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 document.getElementById('btn1').onclick = ()=>{
   alert('Hola Mundo Equipo Uno');
+  console.log(datos);
 }
+
+const datos = [];
+const url = "https://rickandmortyapi.com/api/character"
+function llamar() {
+  
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => { 
+      datos.push(data);
+    });
+}
+
+llamar();
